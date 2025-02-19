fb132b71f185bde592002120e302ba748f65c88c
Adrian <118010000+AdrianIchiro@users.noreply.github.com>
2025-02-19 13:35:16 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('I LOVE U').split('')
+    const titles = ('I LOVE U MAE').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
